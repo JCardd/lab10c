3acdcdec5314ca4c35f938664c284c90808eaeea
JCardd <111624071+JCardd@users.noreply.github.com>
2023-12-04 02:08:22 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My React Web Page</h1>
+        <p>This page was built using Create React App.</p>
+        <p>I installed Create React App by running the commands:</p>
+        <pre>
+          <code>npx create-react-app react-web-page<br></br></code>
+          <code>cd react-web-page<br></br></code>
+          <code>npm start</code>
+        </pre>
+        <p>Then, I modified the content in the <code>src/App.js</code> file to create this page.</p>
+        <p>
+          There were very little difficulties when creating this webpage. <br></br>I was already familiar with using
+          React before so there was not much of a challenge going into this.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
